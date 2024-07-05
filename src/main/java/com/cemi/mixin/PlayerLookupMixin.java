e0d2d92708629375b0112b20ed8420ad1728e451
Maciek <maciejchamera2003@gmail.com>
2024-07-05 23:33:46 +0200

    Updated to loom 1.7 and loader v0.15.11

---
+++ src/main/java/com/cemi/mixin/PlayerLookupMixin.java
@@ -0,0 +1,23 @@
+package com.cemi.mixin;
+
+import java.util.Collection;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.ChunkPos;
+
+@Mixin(PlayerLookup.class)
+public class PlayerLookupMixin {
+
+    @Inject(method = "tracking", at = @At("RETURN"), cancellable = true)
+    private static void tracking(ServerWorld world, ChunkPos pos,
+            CallbackInfoReturnable<Collection<ServerPlayerEntity>> cir) {
+
+        cir.setReturnValue(
+                world.getChunkManager().threadedAnvilChunkStorage.getPlayersWatchingChunk(pos));
+    }
+}
