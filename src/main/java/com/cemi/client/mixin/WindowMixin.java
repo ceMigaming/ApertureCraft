d123dc7820e5c63c39df1fd067c0c0b1cbbc5ba7
ceMigaming <maciejchamera2003@interia.pl>
2026-06-27 14:38:34 +0200

    Fixed an import bug.

--- src/main/java/com/cemi/client/mixin/WindowMixin.java
+++ src/main/java/com/cemi/client/mixin/WindowMixin.java
@@ -1,12 +1,13 @@
 package com.cemi.client.mixin;
 
+import javax.annotation.Nullable;
+
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import blue.endless.jankson.annotation.Nullable;
 import net.minecraft.client.WindowEventHandler;
 import net.minecraft.client.WindowSettings;
 import net.minecraft.client.util.MonitorTracker;
